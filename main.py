012914d7cd3515fa4a36d8c2566445d562219cca
Bruno Brito <79880883+Bruno-Brito-2676@users.noreply.github.com>
2022-11-22 22:38:51 +0000

    Adição GitIgnore Aprendi a criar varáveis e exibi-las na tela!

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 #
 #print() = comando de saida
 print("Hello world!")
+
+# Quando quiser guardar uma String!(Frase)
+nome = "Bruno Brito"
+
+# Quando quiser guardar um número inteiro
+idade = 22
+
+#Exibir o nome (Que está dentro da variável nome)
+print(nome)
+
+#Exibir a idade (Que está dentro da variável idade)
+print(idade)
+
+
+
+
+'''
+Comentario em blovo
+'''
