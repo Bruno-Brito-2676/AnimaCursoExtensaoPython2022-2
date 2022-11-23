3c353533d4829a6e4332adcd727710930f84ccce
Bruno Brito <79880883+Bruno-Brito-2676@users.noreply.github.com>
2022-11-23 00:59:51 +0000

    Adição de códigos em Aula2c. Fim aula 2.

---
+++ Aula2-2022-10-26c.py
@@ -0,0 +1,19 @@
+# Exemplo de laço
+
+#Se eu quisesse exibir números de 1 a 10?
+print('Aqui mostramos de maneira "rústica"')
+print("01")
+print("02")
+print("03")
+print("04")
+print("05")
+print("06")
+print("07")
+print("08")
+print("09")
+print("10\n")
+
+print("Aqui fazemos da maneira correta...")
+numero = 1
+print(numero)
+numero = numero + 1
